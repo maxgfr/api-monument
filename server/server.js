36d69dae83b2dc1e4e335369babf466e267d5c85
Maxime Golfier <maxgolfier@gmail.com>
2018-05-24 09:11:10 +0200

    message

--- server/server.js
+++ server/server.js
@@ -5,18 +5,24 @@ var boot = require('loopback-boot');
 
 var app = module.exports = loopback();
 
-app.start = function() {
-  // start the web server
-  return app.listen(function() {
-    app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
-    if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-    }
-  });
-};
+app.use('/express-status', function(req, res, next) {
+  res.json({ running: true });
+});
+
+if (require.main === module) {
+    app.start = function() {
+      // start the web server
+      return app.listen(function() {
+        app.emit('started');
+        var baseUrl = app.get('url').replace(/\/$/, '');
+        console.log('Web server listening at: %s', baseUrl);
+        if (app.get('loopback-component-explorer')) {
+          var explorerPath = app.get('loopback-component-explorer').mountPath;
+          console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+        }
+      });
+    };
+}
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
