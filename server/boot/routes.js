03bebb15b2ef7069fe235e1f6584eecb4714e9c3
Maxime Golfier <maxgolfier@gmail.com>
2018-05-24 09:11:42 +0200

    message

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -1,7 +1,7 @@
 module.exports = function(app) {
   var router = app.loopback.Router();
   router.get('/', function(req, res) {
-    res.send('pongaroo');
+    res.send('index');
   });
   app.use(router);
 }
